b2b945485c3620d6957c91663a1c2ad4ffdb5c3c
Patrick Tjin <pattjin@google.com>
2019-10-15 10:32:37 +0800

    violet: Add nosdcard characteristic to device
    * Just as per stock.
    
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff

--- device.mk
+++ device.mk
@@ -10,6 +10,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
