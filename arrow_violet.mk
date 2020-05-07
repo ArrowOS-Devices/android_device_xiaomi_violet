ff316d3df70fea020dbb48bbc3b07730fe5b249b
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2020-05-07 23:05:32 +0530

    violet: update coral build fp to may

--- arrow_violet.mk
+++ arrow_violet.mk
@@ -29,6 +29,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet" \
     TARGET_DEVICE="violet"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.A3/6353761:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
