148e05d5ece0ca82a3c9b7189f5a274a4a021c99
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    violet: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,11 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/camera.qcom.so)
+            patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+            patchelf --remove-needed "libmegface.so" "${2}"
+            patchelf --add-needed "libshim_megvii.so" "${2}"
+            ;;
     esac
 }
 
