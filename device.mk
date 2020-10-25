d87f532f9900065966930a46d9b673f75baa4e21
tesla59 <nishant.maintainers@pixysos.com>
2020-10-25 07:01:23 +0000

    Revert "violet: nuke bootjars"
    This reverts commit 52b52bd7ed893880b1bb7b9ea5ba77a1777a64b2.

--- device.mk
+++ device.mk
@@ -406,6 +406,9 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     telephony-ext
 
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
@@ -459,6 +462,9 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libqdMetaData.vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 # Hacc
 PRODUCT_BOARD_PLATFORM := sm6150
