56fa614fa3b12639fd63e0bf0a0b48ee862ff999
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2020-12-10 21:12:33 +0530

    Violet: Opt in for pixel charging animation

--- arrow_violet.mk
+++ arrow_violet.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
 DEVICE_MAINTAINER := Aftab Ashrafi
