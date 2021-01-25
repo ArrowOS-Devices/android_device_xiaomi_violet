9a67c7df09f5299b34888ec401a3f9634cc7f933
Arian <arian.kulmer@web.de>
2021-01-25 12:42:07 +0100

    violet: Rename fingerprint service
    Change-Id: I09992ec66ba4b65bb60a23faa74929d385f199fa

--- device.mk
+++ device.mk
@@ -150,6 +150,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.violet
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
