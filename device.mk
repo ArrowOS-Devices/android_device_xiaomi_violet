d77971057d29e6001e3b7e3754f97f24c3c4c71c
Joker-V2 <nipinna0@gmail.com>
2021-10-09 19:19:54 +0000

    violet: Build netd@1.1

--- device.mk
+++ device.mk
@@ -393,6 +393,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.5 \
     android.hardware.radio.config@1.2.vendor \
+    android.system.net.netd@1.1.vendor \
     android.hardware.secure_element@1.1 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
