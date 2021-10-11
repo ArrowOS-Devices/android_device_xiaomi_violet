0a1c3e4c3d887c03d67f20c57403eebbf11fd0bc
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2021-10-11 22:16:40 +0530

    A12 fp from redfin

--- arrow_violet.mk
+++ arrow_violet.mk
@@ -22,13 +22,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys" \
     PRODUCT_NAME="violet"
     
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
