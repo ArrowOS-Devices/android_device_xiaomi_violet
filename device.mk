30dc07b5bfe7e87609214314883f4693d3ce8397
Joker-V2 <nipinna0@gmail.com>
2021-10-20 00:45:05 +0000

    violet: Switch to AOSP health implementation

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
 # HIDL
