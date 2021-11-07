221963faaf98ad72c13d70ff02d858ca4dc6d62a
raghavt20 <raghavtandon2000@gmail.com>
2021-11-08 00:21:16 +0530

    violet: Provide android.hardware.sensors@2.0 on vendor

--- device.mk
+++ device.mk
@@ -343,6 +343,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0.vendor
+
 # Sensors Configs
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
