bbff5bd62a07afc48ee8ed0763e2c57118d63671
Bruno Martins <bgcngm@gmail.com>
2022-05-02 11:28:36 +0100

    violet: Move to common fingerprint HIDL
    Change-Id: I7cb0a45c865d9dbfd800b1d633f8d5ae613bb1b9

--- device.mk
+++ device.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.violet
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
