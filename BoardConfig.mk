c0cf824bda2bbc80ecc38a160c883978d4cc333e
raghavt20 <raghavt20@aospa.co>
2023-04-16 14:04:53 +0530

    violet: Move to legacy-um qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
